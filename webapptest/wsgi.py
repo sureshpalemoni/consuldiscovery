9ae4a4b040498ce353c4e7bdc1f0c9f1b580d442
Suresh Palemoni <sp@Sureshs-MBP.lan>
2018-11-30 11:46:02 +0400

    Corrected the Project name

--- webapptest/wsgi.py
+++ webapptest/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for relayrproj project.
+WSGI config for webapptest project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "relayrproj.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webapptest.settings")
 
 application = get_wsgi_application()
